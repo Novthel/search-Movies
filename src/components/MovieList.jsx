7c3c9928517e710101896082237a6e9fdda4404c
oscar novoa <novthel@live.com>
2022-04-24 20:38:02 -0500

    soluciona problema de repeticion pagina 1

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -7,14 +7,12 @@ import Spinner from './Spinner';
 
 
 export default function MovieList({search}) {
-
-  const [movies,setMovies] = useState([]);
   const [ page, setPage ] = useState(1);
   const [hasMore, setHasMore] = useState(true);
-  
+  const [movies,setMovies] = useState([]);
 
   useEffect(()=>{
-    const searchURL = search ? '/search/movie/?query=' + search + '&page=' + page : '/discover/movie?page=' + page;
+    const searchURL = search ? '/search/movie?query=' + search + '&page=' + page : '/discover/movie?page=' + page;
     get(searchURL).then((data)=>{
       setMovies(prevMovies =>  prevMovies.concat(data.results));
       setHasMore( data.page < data.total_pages );
